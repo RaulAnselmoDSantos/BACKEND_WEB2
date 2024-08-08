56564ce6d98ba236a85b47d8f02f4d6c3dca309a
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-08-07 22:00:02 -0300

    feat: creation of the models

---
+++ src/models/userModel.js
@@ -0,0 +1,20 @@
+const getAll = () => {
+    return [
+        {
+            id: 1,
+            name: 'Raul',
+            email: 'raul@ifsp.edu.br'
+        },
+        {
+            id: 2,
+            name: 'Raulzito',
+            email: 'raulzito@ifsp.edu.br'
+        }
+    ]
+}
+
+const create = () => {
+
+}
+
+export default {getAll, create}
