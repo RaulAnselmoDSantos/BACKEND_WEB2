bffba6444b0eb8f59981b2efa6e9d7e5c534592d
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-08-21 21:36:26 -0300

    feat: ajustment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT || 3000;
 export const HOST = process.env.HOST 
 export const ENVIRONMENT = process.env.ENVIRONMENT 
+export const TYPE_DATABASE = process.env.TYPE_DATABASE
